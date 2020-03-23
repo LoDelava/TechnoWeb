40c0efac9d63fbed9bd50f8ac935269e1fd9e078
LoDelava <55684493+LoDelava@users.noreply.github.com>
2020-03-23 14:46:47 +0100

    Add files via upload

--- Accueil.vue
+++ Accueil.vue
@@ -3,8 +3,11 @@
             <router-link class="nav-button" :to="{ name: 'Accueil'}"><h3>Accueil</h3></router-link>
             <router-link class="nav-button" :to="{ name: 'Formulaire'}"><h3>Formulaire</h3></router-link>
             <router-link class="nav-button" :to="{ name: 'About'}"><h3>About</h3></router-link>
-            <div></div>
+            <h1> Bienvenue </h1>
         </div>
+        
+
+        
 </template>
 <script>
 export default {
